bbceba5b9e5e0440e7014d2f6a6fdeb246628e8f
CombatPug <pe.ladar@icloud.com>
2024-07-29 17:58:46 +0200

    update types for new more generic serviceQueue approach

--- src/p2p/ServiceQueueTypes.ts
+++ src/p2p/ServiceQueueTypes.ts
@@ -1,15 +1,13 @@
-import {publicKey} from "../index";
-
 export interface Txs {
 }
 
-export interface ServiceEntry {
-  publicKey: publicKey
-  startCycle: number
-  endCycle?: number
-  rewardRate: number
+export interface NetworkTx {
+  type: string
+  txData: any // todo: better type here?
 }
 
 export interface Record {
-  serviceQueue: ServiceEntry[]
+  txadd: NetworkTx[]
+  txremove: string[]
+  txlisthash: string
 }
